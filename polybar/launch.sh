abc1835e0d7bf2d803a985d538175da47687c6ec
mrtaichi <isaaniet@gmail.com>
2024-12-27 21:20:45 -0600

    Desktop: Polybar: Fix for multiple monitors in xworkspaces. Launch script corrected

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,8 +2,8 @@
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
+    MONITOR=$m polybar --reload example --config=~/.config/polybar/config.ini&
   done
 else
-  polybar --reload example &
+  polybar --reload example --config=~/.config/polybar/config.ini&
 fi
