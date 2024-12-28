ca293831bff4cc453354dcbf47f619cde2478f39
mrtaichi <isaaniet@gmail.com>
2024-12-27 20:44:37 -0600

    Desktop and laptop: Polybar launch fix for multiple monitors and desktop usage

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
-polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
-# killall -q polybar
-
-# Launch bar1 and bar2
-polybar --config=~/.config/polybar/config.ini example 2>&1 & disown
-
-echo "Bars launched..."
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
