454d7850a9ba9e549c3b6e8a52697f81ccc30cc3
mrtaichi <isaaniet@gmail.com>
2025-11-16 02:21:26 -0600

    feat: Removes not tangled files

--- polybar/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example --config=~/.config/polybar/config.ini&
-  done
-else
-  polybar --reload example --config=~/.config/polybar/config.ini&
-fi
