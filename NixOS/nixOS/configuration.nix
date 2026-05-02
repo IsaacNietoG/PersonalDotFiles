788ceef3bd9e8d849bfb267c87bbb32cae0f2e80
mrtaichi <isaaniet@gmail.com>
2026-05-01 19:59:24 -0600

    feat: private configs

--- NixOS/nixOS/configuration.nix
+++ NixOS/nixOS/configuration.nix
@@ -4,6 +4,7 @@
   imports =
     [ # Include the results of the hardware scan.
       /etc/nixos/hardware-configuration.nix
+      ./private.nix
       inputs.hyprland.nixosModules.default
     ];
   fileSystems = {
